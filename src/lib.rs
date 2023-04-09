ef57f25c4541915048eb602a59ffd5167a5d24b0
Bryce Whitney <brycewhit13@gmail.com>
2023-04-08 23:02:39 -0400

    similarity functionality implemented

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,34 @@
 // Crates
-use sbert::SBertRT;
-use std::path::PathBuf;
-use std::env::current_dir;
+use reqwest;
+use serde_json::json;
 
 // Functions
-pub fn sentence_similarity(prompt1: String, prompt2: String) {
-    // Get the path to the weights
-    let mut home: PathBuf = current_dir().unwrap();
-    home.push("models");
-    home.push("distiluse-base-multilingual-cased");
+pub async fn sentence_similarity(prompt1: &str, prompt2: &str) {
+    let model = "all-MiniLM-L6-v2";
+    let url = format!("https://api-inference.huggingface.co/models/sentence-transformers/{model}");
+    // Instantiate the payload
+    let payload = json!({
+        "inputs": json!({
+            "source_sentence": prompt1,
+            "sentences": [prompt2]
+        })
+    });
 
-    // Initialize the model
-    let sbert_model = SBertRT::new(home.to_str().unwrap()).unwrap();
+    // Instantiate the client and send the request
+    let client = reqwest::Client::new();
+    let res = client.post(url)
+        .json(&payload)
+        .send()
+        .await
+        .unwrap();
+        
+    // Extract the similarity and round it to 2 decimal places
+    let body = res.json::<serde_json::Value>().await.unwrap();
+    let similarity = &body[0].as_f64().unwrap();
+    let similarity = (similarity * 10000.0).round() / 100.0;
 
-    // Encode the sentences
-    let texts = vec![prompt1, prompt2];
-    let batch_size = 8;
-    let output = sbert_model.encode(&texts.to_vec(), batch_size).unwrap();
+    // Print the response
+    println!("Prompt 1: {}", prompt1);
+    println!("Prompt 2: {}", prompt2);
+    println!("Similarity: {}%", similarity);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 // Crates
 use sentence_similarity_rs::sentence_similarity;
 
-fn main() {
-    let p1 = "Hello World!";
-    let p2 = "Hello Rust!";
-    sentence_similarity(p1.to_string(), p2.to_string());
+#[tokio::main]
+async fn main() {
+    let prompt1 = "Hello Rust!";
+    let prompt2 = "Hello World!";
+    sentence_similarity(prompt1, prompt2).await;
 }
